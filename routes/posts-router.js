3ad7c062f17149a068cbc8021feb8b2c73ab3ced
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-08-04 15:56:10 -0500

    🛠 POST:  api/posts/ --> create new post

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../data/db");
 
+// GET all posts
 router.get("/", async (req, res) => {
   try {
     const posts = await db.find();
@@ -14,13 +15,13 @@ router.get("/", async (req, res) => {
       .json({ error: "The posts information could not be retrieved." });
   }
 });
-
+// GET post by :id
 router.get("/:id", async (req, res) => {
   try {
     const id = req.params.id;
     const foundPost = await db.findById(id);
     foundPost.length < 1
-      ? res.status(400).json({ error: `post id:${id} does not exist` })
+      ? res.status(404).json({ error: `post id:${id} does not exist` })
       : res.status(200).json({ ...foundPost });
   } catch {
     res
@@ -28,7 +29,7 @@ router.get("/:id", async (req, res) => {
       .json({ error: "The post information could not be retrieved." });
   }
 });
-
+// GET comments by post :id
 router.get("/:id/comments", async (req, res) => {
   try {
     const id = req.params.id;
@@ -42,6 +43,25 @@ router.get("/:id/comments", async (req, res) => {
     res
       .status(500)
       .json({ error: "The comments information could not be retrieved." });
+  }
+});
+
+// POST a new post (title + contents required)
+router.post("/", async (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post.",
+    });
+  }
+
+  try {
+    const newPost = await db.insert(req.body);
+    res.status(201).json({ ...newPost, ...req.body });
+  } catch {
+    res.status(500).json({
+      error: "There was an error while saving the post to the database",
+    });
   }
 });
 
