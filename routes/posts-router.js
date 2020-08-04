dbcd0f55210d678ec87658a23fd500869fddbcc3
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-08-04 15:00:49 -0500

    🛠 GET /:id/comments --> comments from post id

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -21,11 +21,27 @@ router.get("/:id", async (req, res) => {
     const foundPost = await db.findById(id);
     foundPost.length < 1
       ? res.status(400).json({ error: `post id:${id} does not exist` })
-      : res.json({ ...foundPost });
+      : res.status(200).json({ ...foundPost });
   } catch {
     res
       .status(500)
       .json({ error: "The post information could not be retrieved." });
+  }
+});
+
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const foundPostComments = await db.findPostComments(id);
+    foundPostComments.length < 1
+      ? res.status(400).json({
+          message: `Post id:${id} has no comments or doesn't exist at all.`,
+        })
+      : res.status(200).json({ ...foundPostComments });
+  } catch {
+    res
+      .status(500)
+      .json({ error: "The comments information could not be retrieved." });
   }
 });
 
